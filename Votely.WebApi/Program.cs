6b5adebc7816a67cbb26925233f82a96e6833c03
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-04-25 11:07:25 -0300

    chore: configurate secret for db connection

--- Votely.WebApi/Program.cs
+++ Votely.WebApi/Program.cs
@@ -2,9 +2,12 @@ using Votely.Application.Survey.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("SqlServerConnectionString");
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<VotelyDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<ISurveyService, SurveyService>();
 
 builder.Services.AddControllers();

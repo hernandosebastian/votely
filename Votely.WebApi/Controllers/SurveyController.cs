e72599a6673e059a8cd5188bac2b10ca7a6bbb83
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-04-25 15:24:15 -0300

    feature: add repository and update controller & service logic based on it

--- Votely.WebApi/Controllers/SurveyController.cs
+++ Votely.WebApi/Controllers/SurveyController.cs
@@ -1,11 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
 using Votely.Application.Surveys;
 using Votely.Application.Surveys.DTOs;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Votely.WebApi.Controllers;
 
 [ApiController]
-[Route("api/survey")]
+[Route("api/surveys")]
 public class SurveyController : ControllerBase
 {
     private readonly ISurveyService _surveyService;
@@ -25,49 +25,63 @@ public class SurveyController : ControllerBase
         }
         catch (Exception e)
         {
-            return Problem(e.Message);
+            return NotFound(e.Message);
         }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllSurveys()
+    {
+        var result = await _surveyService.GetAllSurveysAsync();
+        return Ok(result);
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateSurvey([FromBody] CreateSurveyDto dto)
     {
-        try
-        {
-            var result = await _surveyService.CreateSurveyAsync(dto);
-            return Ok(result);
-        }
-        catch (Exception e)
-        {
-            return Problem(e.Message);
-        }
+        var result = await _surveyService.CreateSurveyAsync(dto);
+        return CreatedAtAction(nameof(GetSurvey), new { id = result.Id }, result);
     }
 
-    [HttpPost("{surveyId}/question")]
+    [HttpPost("{surveyId}/questions")]
     public async Task<IActionResult> AddQuestion(Guid surveyId, [FromBody] CreateQuestionDto dto)
     {
-        try
-        {
-            var result = await _surveyService.AddQuestionAsync(surveyId, dto);
-            return Ok(result);
-        }
-        catch (Exception e)
-        {
-            return Problem(e.Message);
-        }
+        var result = await _surveyService.AddQuestionAsync(surveyId, dto);
+        return Ok(result);
     }
 
-    [HttpPost("{surveyId}/question/{questionId}/option")]
+    [HttpPost("{surveyId}/questions/{questionId}/options")]
     public async Task<IActionResult> AddOption(Guid surveyId, Guid questionId, [FromBody] CreateOptionDto dto)
+    {
+        var result = await _surveyService.AddOptionAsync(surveyId, questionId, dto);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteSurvey(Guid id)
     {
         try
         {
-            var result = await _surveyService.AddOptionAsync(surveyId, questionId, dto);
-            return Ok(result);
+            await _surveyService.DeleteSurveyAsync(id);
+            return NoContent();
         }
         catch (Exception e)
         {
-            return Problem(e.Message);
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPost("{surveyId}/questions/{questionId}/options/{optionId}/vote")]
+    public async Task<IActionResult> Vote(Guid surveyId, Guid questionId, Guid optionId)
+    {
+        try
+        {
+            await _surveyService.VoteOptionAsync(surveyId, questionId, optionId);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
         }
     }
 }
